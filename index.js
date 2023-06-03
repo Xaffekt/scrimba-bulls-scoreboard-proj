c5ca28f83a28b2c78c6a3b8f6eec65e14b534151
Xaffekt <134972321+Xaffekt@users.noreply.github.com>
2023-06-03 10:36:47 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+let homeScore = document.getElementById("home-score");
+let guestScore = document.getElementById("guest-score");
+
+let homeScoreTotal = 0;
+let guestScoreTotal = 0;
+
+function addScore(button)
+{
+    let x = button.id;  
+    console.log(x); 
+
+    switch(x) //uses button ID to add the correct amount to the correct team
+    {
+        case "home-1":
+            homeScoreTotal += 1;
+            homeScore.textContent = homeScoreTotal;
+            break;
+        case "home-2":
+            homeScoreTotal += 2;
+            homeScore.textContent = homeScoreTotal;
+            break;
+        case "home-3":
+            homeScoreTotal += 3;
+            homeScore.textContent = homeScoreTotal;
+            break;
+        case "guest-1":
+            guestScoreTotal += 1;
+            guestScore.textContent = guestScoreTotal;
+            break;
+        case "guest-2":
+            guestScoreTotal += 2;
+            guestScore.textContent = guestScoreTotal;
+            break;
+        case "guest-3":
+            guestScoreTotal += 3;
+            guestScore.textContent = guestScoreTotal;
+            break;
+    }
+}
+
+function reset()
+{
+    homeScore.textContent = 0;
+    guestScore.textContent = 0;
+}
